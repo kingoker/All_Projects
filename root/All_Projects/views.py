43a8c76fd6a7bae368e34ac49c939db9cf82ee0a
king_ok_er <zolotojznak@gmail.com>
2022-10-31 17:51:13 +0500

    first commit

---
+++ root/All_Projects/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def home(request):
+    pass
+
+    return render(request, 'All_Projects/home.html')
---
+++ root/Brands/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def main(request):
+    pass
+    
+    return render(request, 'Brands/index.html')
